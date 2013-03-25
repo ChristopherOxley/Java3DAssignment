978d7cf095770231b13bfe4bcf3731ca4597f994
Christopher Oxley <chrisoxley@me.com>
2013-03-25 19:07:54 +0000

    Cleaned up formatting

--- Java3DAssignment/src/java3dassignment/ThemePark.java
+++ Java3DAssignment/src/java3dassignment/ThemePark.java
@@ -11,120 +11,109 @@ import javax.vecmath.*;
 import java.io.*;
 import java.awt.*;
 import com.sun.j3d.utils.behaviors.mouse.*;
-import com.sun.j3d.utils.behaviors.keyboard.*; 
+import com.sun.j3d.utils.behaviors.keyboard.*;
 import javax.swing.JApplet;
 import com.sun.j3d.utils.image.TextureLoader;
 import java.net.*;
 import com.sun.j3d.loaders.objectfile.ObjectFile;
 import com.sun.j3d.loaders.*;
 
+class ThemePark extends JApplet {
+
+    // Set the bounding sphere for the application
+    static public final BoundingSphere bounds = new BoundingSphere(new Point3d(), 1000.0);
+
+    // My Picture
+    public TransformGroup createPicture() {
+
+        // Create a main transofrm group
+        TransformGroup mainTG = new TransformGroup();
+
+        TransformGroup carousel = Carousel.createCarousel();
+
+        mainTG.addChild(carousel);
+
+        return mainTG;
+    }
+
+    public BranchGroup createSceneGraph(SimpleUniverse su) {
+
+        // creating the universe
+        BranchGroup objRoot = new BranchGroup();
+
+        // Set up the global lights
+        Color3f lColor1 = new Color3f(0.7f, 0.7f, 0.7f);
+        Vector3f lDir1 = new Vector3f(-1.0f, -1.0f, -1.0f);
+        Color3f alColor = new Color3f(1.0f, 1.0f, 1.0f);
 
 
-class ThemePark extends JApplet{
-	
-	
-	
-		// Set the bounding sphere for the application
-	    static public final BoundingSphere bounds = new BoundingSphere(new Point3d(), 1000.0);
+        AmbientLight aLgt = new AmbientLight(alColor);
+        aLgt.setInfluencingBounds(bounds);
+        DirectionalLight lgt1 = new DirectionalLight(lColor1, lDir1);
+        lgt1.setInfluencingBounds(bounds);
+        objRoot.addChild(aLgt);
+        objRoot.addChild(lgt1);
+
+        // adding the TG of the picture to the universe
+        objRoot.addChild(createPicture());
+
+        // adding mouse and keyboard controls
+        addControls(su, objRoot);
+
+        objRoot.compile();
+
+        return objRoot;
+    }
+
+    // Standard Controls
+    public void addControls(SimpleUniverse su, BranchGroup bg) {
+        TransformGroup cameraTG = su.getViewingPlatform().
+                getViewPlatformTransform();
+
+        // starting postion of the viewing platform
+        Vector3f translate = new Vector3f();
+        Transform3D T3D = new Transform3D();
+        translate.set(0.0f, 0.3f, 0.0f);
+        T3D.setTranslation(translate);
+        cameraTG.setTransform(T3D);
+
+        // Create the key behavior node
+        KeyNavigatorBehavior keyBehavior = new KeyNavigatorBehavior(cameraTG);
+        keyBehavior.setSchedulingBounds(bounds);
+        bg.addChild(keyBehavior);
+
+        // Create the rotate behavior node
+        MouseRotate behavior = new MouseRotate(MouseBehavior.INVERT_INPUT);
+        behavior.setTransformGroup(cameraTG);
+        behavior.setSchedulingBounds(bounds);
+        bg.addChild(behavior);// 
+
+        // Create the zoom behavior node
+        MouseZoom behavior2 = new MouseZoom(MouseBehavior.INVERT_INPUT);
+        behavior2.setTransformGroup(cameraTG);
+        behavior2.setSchedulingBounds(bounds);
+        bg.addChild(behavior2);
 
 
+        // Create the translate behavior node
+        MouseTranslate behavior3 = new MouseTranslate(MouseBehavior.INVERT_INPUT);
+        behavior3.setTransformGroup(cameraTG);
+        behavior3.setSchedulingBounds(bounds);
+        bg.addChild(behavior3);
+    }
 
-		// My Picture
-	    public TransformGroup createPicture() {
-		
-			// Create a main transofrm group
-			TransformGroup mainTG = new TransformGroup();           
-		
-                        TransformGroup carousel = Carousel.createCarousel();
-			
-                        mainTG.addChild(carousel);
-                        
-			return mainTG;		
-		}
-		
-		
-	    public BranchGroup createSceneGraph(SimpleUniverse su) {
-		
-			// creating the universe
-			BranchGroup objRoot = new BranchGroup();
+    ThemePark() {
+        Container cp = getContentPane();
+        cp.setLayout(new BorderLayout());
 
-			// Set up the global lights
-			Color3f lColor1 = new Color3f(0.7f, 0.7f, 0.7f);
-			Vector3f lDir1  = new Vector3f(-1.0f, -1.0f, -1.0f);
-			Color3f alColor = new Color3f(1.0f, 1.0f, 1.0f);
-
-			
-			AmbientLight aLgt = new AmbientLight(alColor);
-			aLgt.setInfluencingBounds(bounds);
-			DirectionalLight lgt1 = new DirectionalLight(lColor1, lDir1);
-			lgt1.setInfluencingBounds(bounds);
-			objRoot.addChild(aLgt);
-			objRoot.addChild(lgt1);
-			
-			// adding the TG of the picture to the universe
-			objRoot.addChild(createPicture());
-
-			// adding mouse and keyboard controls
-			addControls(su,objRoot);
-
-			objRoot.compile();
-			
-			return objRoot;	
-		}
-
-
-		// Standard Controls
-		public void addControls(SimpleUniverse su, BranchGroup bg) {
-			TransformGroup cameraTG = su.getViewingPlatform().
-			                            getViewPlatformTransform();
-
-			// starting postion of the viewing platform
-			Vector3f translate = new Vector3f();
-			Transform3D T3D = new Transform3D();
-			translate.set( 0.0f, 0.3f, 0.0f);
-		    T3D.setTranslation(translate);
-			cameraTG.setTransform(T3D);
-
-			// Create the key behavior node
-		    KeyNavigatorBehavior keyBehavior = new 
-			KeyNavigatorBehavior(cameraTG);
-			keyBehavior.setSchedulingBounds(bounds);
-		    bg.addChild(keyBehavior);
-			
-		 	// Create the rotate behavior node
-		 	MouseRotate behavior = new MouseRotate(MouseBehavior.INVERT_INPUT);
-		 	behavior.setTransformGroup(cameraTG);
-		 	behavior.setSchedulingBounds(bounds);
-		 	bg.addChild(behavior);// 
-
-			// Create the zoom behavior node
-		 	MouseZoom behavior2 = new MouseZoom(MouseBehavior.INVERT_INPUT);
-		 	behavior2.setTransformGroup(cameraTG);
-		 	behavior2.setSchedulingBounds(bounds);
-			bg.addChild(behavior2);
-		 
-			
-		 	// Create the translate behavior node
-		 	 MouseTranslate behavior3 = new MouseTranslate(MouseBehavior.INVERT_INPUT);
-		 	behavior3.setTransformGroup(cameraTG);
-		 	behavior3.setSchedulingBounds(bounds);
-		 	bg.addChild(behavior3);	
-		}
-
-	ThemePark(){
-		Container cp = getContentPane();
-		cp.setLayout(new BorderLayout());
-		
-		GraphicsConfigTemplate3D template = new GraphicsConfigTemplate3D();
-		template.setSceneAntialiasing(GraphicsConfigTemplate.REQUIRED);
-		GraphicsConfiguration config = GraphicsEnvironment.getLocalGraphicsEnvironment().
-		getDefaultScreenDevice().getBestConfiguration(template);		
-		Canvas3D c = new Canvas3D(config, false);
-		cp.add("Center", c);
-		SimpleUniverse u = new SimpleUniverse(c);
-		BranchGroup scene = createSceneGraph(u);
-		u.addBranchGraph(scene);
-	}
-
-
+        GraphicsConfigTemplate3D template = new GraphicsConfigTemplate3D();
+        template.setSceneAntialiasing(GraphicsConfigTemplate.REQUIRED);
+        GraphicsConfiguration config = GraphicsEnvironment.getLocalGraphicsEnvironment().
+                getDefaultScreenDevice().getBestConfiguration(template);
+        Canvas3D c = new Canvas3D(config, false);
+        cp.add("Center", c);
+        SimpleUniverse u = new SimpleUniverse(c);
+        BranchGroup scene = createSceneGraph(u);
+        u.addBranchGraph(scene);
+    }
 }
